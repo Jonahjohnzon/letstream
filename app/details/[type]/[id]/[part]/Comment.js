0dd437e0f5653b3581714c7b666c179780b221f1
jonahjohnzon <jonahjohnzon@gmail.com>
2026-08-10 07:14:33 -0400

    push

--- app/details/[type]/[id]/[part]/Comment.js
+++ app/details/[type]/[id]/[part]/Comment.js
@@ -2,7 +2,9 @@ import React from 'react'
 import TitleBar from '@/app/Component/TitleBar'
 import YouTube from 'react-youtube'
 
-const opts = { height: '400', width: '100%', playerVars: { autoplay: 0, controls: 0, rel: 0 } }
+const opts = {
+  playerVars: { autoplay: 0, controls: 0, rel: 0 },
+}
 const onReady = (event) => event.target.pauseVideo()
 
 const TrailerBox = ({ url }) => {
@@ -11,9 +13,17 @@ const TrailerBox = ({ url }) => {
       <section className='w-[95%] sm:w-[445px] md:w-[645px] lg:w-[745px]'>
         <TitleBar title={"Trailer Box"} />
         <section className='min-h-96 w-full mt-5 flex justify-center'>
-          <div className='border-white border-[1px] h-[402px] w-[95%] md:w-[70%] lg:w-[745px] flex items-center justify-center'>
+          <div className='border-white border-[1px] w-[95%] md:w-[70%] lg:w-[745px] flex items-center justify-center'>
             {url ? (
-              <YouTube videoId={url} opts={opts} onReady={onReady} />
+              <div className='relative w-full aspect-video'>
+                <YouTube
+                  videoId={url}
+                  opts={opts}
+                  onReady={onReady}
+                  className='absolute inset-0 w-full h-full'
+                  iframeClassName='absolute inset-0 w-full h-full'
+                />
+              </div>
             ) : (
               <p className='text-gray-400 font-semibold'>No trailer available</p>
             )}
